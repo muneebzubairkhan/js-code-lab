0634c463df11bd4efdc835df924b388470ade5e1
Muneeb <muneeb.zubair.hash@gmail.com>
2022-05-05 22:21:53 +0500

    pv key

---
+++ parsePV.js
@@ -0,0 +1,31 @@
+const removeDuplicates = true;
+
+// dependencies
+const fs = require('fs');
+
+// input
+const data = fs.readFileSync('ethAddressesIn.txt', 'utf8');
+
+// read only the addresses from file
+let addresses = data.match(/(\b0x[a-fA-F0-9]{64}\b)/g);
+const addrLen = addresses.length;
+
+// remove duplicates
+if (removeDuplicates) {
+  let betterRes = {};
+  addresses.map(a => (betterRes[a] = true));
+  addresses = Object.keys(betterRes);
+}
+
+
+console.log(JSON.stringify(addresses, null, 4).replaceAll("\n", "").replaceAll("   ", ""));
+// console.log(JSON.stringify(addresses, null, 4));
+// output
+// fs.writeFile(
+//   'ethAddressesOut.txt',
+//   `total addresses were: ${addrLen}
+//   ${addresses.length} 'unique addresses were found. Thanks to Allah.'
+  
+//   ${addresses}`,
+//   e => e && console.log(e.message),
+// );
